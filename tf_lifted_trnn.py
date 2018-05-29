4588f875b7f93c1ecfbd9fac3567847acd5e8d69
Alec Brickner <alecjbrick@hotmail.com>
2018-05-29 02:10:13 -0700

    Fix bug in embed_dim being a Dimension instead of int

--- tf_lifted_trnn.py
+++ tf_lifted_trnn.py
@@ -32,6 +32,11 @@ class TfTreeRNNClassifier(TfModelBase):
 
     def build_graph(self):
         self._define_embedding()
+        # hacky but w/e
+        try:
+            self.embed_dim = self.embed_dim.value
+        except:
+            pass
 
         # The inputs
         self.inputs = tf.placeholder(
@@ -77,8 +82,8 @@ class TfTreeRNNClassifier(TfModelBase):
         self.b_lstm = self.bias_init(
             4 * self.hidden_dim_v, 'b_lstm')
         self.W_comb = self.weight_init(self.hidden_dim, self.hidden_dim, 'W_comb')
-        self.b_comb1 = self.weight_init(self.hidden_dim, self.hidden_dim, 'b_comb1')
-        self.b_comb2 = self.weight_init(self.hidden_dim, self.hidden_dim, 'b_comb2')
+        self.b_comb = self.weight_init(self.hidden_dim, self.hidden_dim, 'b_comb')
+        #self.b_comb2 = self.weight_init(self.hidden_dim, self.hidden_dim, 'b_comb2')
         # maybe xavier init here
         self.c_left = self.bias_init(self.hidden_dim_v, 'c_left')
         self.c_right = self.bias_init(self.hidden_dim_v, 'c_left')
@@ -90,40 +95,7 @@ class TfTreeRNNClassifier(TfModelBase):
         #node_tensors = node_tensors.write(0, [self.lifted_feats_t[0], self.lifted_feats_t[0]])
         #x = node_tensors.gather([0])
         
-        # Because there's no generalized diag operation.
-        # Why is there no generalized diag operation? Sad state of affairs!
-        def outer_diag(tensor):
-            trans = tf.transpose(tensor)
-            diag = tf.matrix_diag_part(trans)
-            return tf.transpose(diag)
 
-        # From 224D github
-        # probably bad. update later
-        # [batch] + left_child_dims
-        # oh heck we need to keep track of state!
-        def combine_children(left_tensor, right_tensor):
-            left_C = outer_diag(left_tensor)[:, 0]
-            left_H = outer_diag(left_tensor)[:, 1]
-            right_C = outer_diag(right_tensor)[:, 0]
-            right_H = outer_diag(right_tensor)[:, 1]
-            h_c_left = tf.reshape(left_H, [-1, self.hidden_dim_v])
-            h_c_right = tf.reshape(right_H, [-1, self.hidden_dim_v])
-            c_left = tf.reshape(left_C, [-1, self.hidden_dim_v])
-            c_right = tf.reshape(right_C, [-1, self.hidden_dim_v])
-
-            # LSTM calculations
-            h_concat = tf.concat([h_c_left, h_c_right], 1)
-            z_lstm = tf.matmul(h_concat, self.W_lstm) + self.b_lstm
-            sig_lstm = tf.sigmoid(z_lstm)
-            i, f_l, f_r, o = tf.split(sig_lstm, 4, axis=1)
-            H_inner = tf.tanh(tf.tensordot(left_H, self.W_comb, [2, 0]) + self.b_comb1)
-            # H_inner is now batch - size [?, d, d]
-            H_cand = tf.tanh(tf.matmul(right_H, H_inner) + self.b_comb2)
-            g = tf.reshape(H_cand, [-1, self.hidden_dim_v])
-            c = f_l * c_left + f_r * c_right + i * g
-            H = tf.reshape(o * c, [-1, self.hidden_dim, self.hidden_dim])
-            C = tf.reshape(c, [-1, self.hidden_dim, self.hidden_dim])
-            return tf.stack([C, H], axis=1)
 
         # From 224D github
         # Loop through the tensors, combining them
@@ -139,7 +111,7 @@ class TfTreeRNNClassifier(TfModelBase):
                 # the things i do for batching
                 tf.cond(tf.equal(i, 0),
                     lambda: tf.stack([tf.zeros_like(self.lifted_feats_t[0]), tf.gather(self.lifted_feats_t, i)], axis=1),
-                    lambda: combine_children(node_tensors.gather(left_child),
+                    lambda: self.combine_children(node_tensors.gather(left_child),
                                      node_tensors.gather(right_child))))
             node_tensors = node_tensors.write(i, node_tensor)
             i = tf.add(i, 1)
@@ -154,7 +126,7 @@ class TfTreeRNNClassifier(TfModelBase):
 
         # Get the last [C, H], and retrieve H from that.
         last_pair = node_tensors.gather(self.input_lens)
-        last_H = outer_diag(last_pair)[:, 1]
+        last_H = self.get_last_val(last_pair) # allow for inheritance
         self.last = tf.reshape(last_H, [-1, self.hidden_dim_v])
         self.W_hy = self.weight_init(
             self.hidden_dim_v, self.output_dim, 'W_hy')
@@ -248,7 +220,7 @@ class TfTreeRNNClassifier(TfModelBase):
         padded_inputs = np.zeros((len(input_arrays), len(words), self.max_length), dtype='int')
         # ex_lengths = []
         index = dict(zip(self.vocab, range(len(self.vocab))))
-        unk_index = index['$UNK']
+        unk_index = index['unk']
         # oh you gotta pad your inputs =/
         # And cut them off if they're too long!
         # But that's super difficult for trees, since they're so
@@ -268,6 +240,46 @@ class TfTreeRNNClassifier(TfModelBase):
     def get_optimizer(self):
         return tf.train.AdamOptimizer(
             self.eta).minimize(self.cost)
+
+    # Because there's no generalized diag operation.
+    # Why is there no generalized diag operation? Sad state of affairs!
+    def outer_diag(self, tensor):
+        trans = tf.transpose(tensor)
+        diag = tf.matrix_diag_part(trans)
+        return tf.transpose(diag)
+
+    # From 224D github
+    # probably bad. update later
+    # [batch] + left_child_dims
+    # oh heck we need to keep track of state!
+    def combine_children(self, left_tensor, right_tensor):
+        left_C = self.outer_diag(left_tensor)[:, 0]
+        left_H = self.outer_diag(left_tensor)[:, 1]
+        right_C = self.outer_diag(right_tensor)[:, 0]
+        right_H = self.outer_diag(right_tensor)[:, 1]
+        h_c_left = tf.reshape(left_H, [-1, self.hidden_dim_v])
+        h_c_right = tf.reshape(right_H, [-1, self.hidden_dim_v])
+        c_left = tf.reshape(left_C, [-1, self.hidden_dim_v])
+        c_right = tf.reshape(right_C, [-1, self.hidden_dim_v])
+
+        # LSTM calculations
+        h_concat = tf.concat([h_c_left, h_c_right], 1)
+        z_lstm = tf.matmul(h_concat, self.W_lstm) + self.b_lstm
+        sig_lstm = tf.sigmoid(z_lstm)
+        i, f_l, f_r, o = tf.split(sig_lstm, 4, axis=1)
+        H_inner = tf.matmul(right_H, left_H)
+        H_cand = tf.tanh(tf.tensordot(H_inner, self.W_comb, [2, 0]) + self.b_comb)
+        # H_cand is now batch - size [?, d, d]
+        # H_cand = tf.tanh(tf.matmul(right_H, H_inner) + self.b_comb2)
+        g = tf.reshape(H_cand, [-1, self.hidden_dim_v])
+        c = f_l * c_left + f_r * c_right + i * g
+        H = tf.reshape(o * c, [-1, self.hidden_dim, self.hidden_dim])
+        C = tf.reshape(c, [-1, self.hidden_dim, self.hidden_dim])
+        return tf.stack([C, H], axis=1)
+
+    def get_last_val(self, last_val):
+        last_H = self.outer_diag(last_val)[:, 1]
+        return last_H
 
 def traverse(tree, i):
     is_leaf = []
