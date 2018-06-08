a7b1c76bd1795e4bc9b793ca4f388adba9862ee7
Alec Brickner <alecjbrick@gmail.com>
2018-06-08 11:48:15 +0000

    Experiments

--- run_trnn_exp.py
+++ run_trnn_exp.py
@@ -8,14 +8,31 @@ import tf_lifted_trnn
 import time
 import utils
 
-vsmdata_home = 'vsmdata'
-glove_home = os.path.join(vsmdata_home, 'glove.6B')
-glove_lookup = utils.glove2dict(
-    os.path.join(glove_home, 'glove.6B.50d.txt'))
+vsmdata_home = 'glove.6B'
+glove_home = vsmdata_home
+glove_lookup_100 = utils.glove2dict(
+    os.path.join(glove_home, 'glove.6B.100d.txt'))
+glove_lookup_200 = utils.glove2dict(
+    os.path.join(glove_home, 'glove.6B.200d.txt'))
 
-def main():
+def run_experiment(eta, embed, model, phrase):
+    print ("===================================")
+    print ("eta: %s, embed_dim: %s, model: %s" % (eta, embed, model))
+    print ("===================================")
+
+    if embed == 100:
+        glove_lookup = glove_lookup_100
+    else:
+        glove_lookup = glove_lookup_200
+
+    if model == "lifted":
+        base_model = tf_lifted_trnn.TfLiftedTreeRNNClassifier
+    else:
+        base_model = tf_trnn.TfTreeRNNClassifier
+
+    start = time.time()
     train = sst.build_dataset(
-            sst.train_reader, lambda x: x, sst.ternary_class_func, vectorizer=None, vectorize=False)
+            sst.train_reader, lambda x: x, sst.binary_class_func, vectorizer=None, vectorize=False, subtree_labels=phrase)
     # Manage the assessment set-up:
     X_train = train['X']
     y_train = train['y']
@@ -25,27 +42,40 @@ def main():
     assess = sst.build_dataset(
         sst.dev_reader,
         lambda x: x,
-        sst.ternary_class_func,
+        sst.binary_class_func,
         vectorizer=train['vectorizer'],
-        vectorize=False)
+        vectorize=False, subtree_labels=phrase)
     X_assess, y_assess = assess['X'], assess['y']
 
     tree_vocab = ["unk"] + sst.get_vocab(map(lambda x: x.leaves(), X_train), 
-                                         n_words=3000)
+                                         n_words=5000)
     embedding = np.asarray(
         [glove_lookup.get(k, glove_lookup["unk"]) for k in tree_vocab])
-    model = tf_trnn.TfTreeRNNClassifier(
+    model = base_model(
         tree_vocab, 
-        eta=0.0001,
-        batch_size=32,
-        embed_dim=50,
-        hidden_dim=50,
-        max_length=200,
-        max_iter=25,
+        eta=eta,
+        batch_size=16,
+        embed_dim=embed,
+        hidden_dim=embed,
+        max_length=120,
+        max_iter=10,
         embedding=embedding,
-        train_embedding=True)
+        train_embedding=True,
+        use_phrases=phrase)
     model.fit(X_train, y_train, X_assess=X_assess, y_assess=y_assess)
     end = time.time()
+    print("Time: %s" % (end - start))
+
+def main():
+    params = {
+        "eta": [0.0001, 0.001, 0.01],
+        "embed": [100],
+        "model": ["lifted", "normal"]
+    }
+    for em in params["embed"]:
+        for m in params["model"]:
+            for e in params["eta"]:
+                run_experiment(e, em, m, False)
 
 if __name__ == "__main__":
     main()
